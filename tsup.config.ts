a153812324707b3f82346d85c3d869fbf74c8249
Hendra Gunawan <48410066+JadlionHD@users.noreply.github.com>
2025-05-28 15:30:18 +0700

    1.0.0

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,4 +1,8 @@
 import { defineConfig } from "tsup";
+import { readFile, readFileSync } from "fs";
+
+const license = readFileSync("./LICENSE", "utf-8");
+const pkg = JSON.parse(readFileSync("./package.json", "utf-8")) as Record<string, string>;
 
 export default defineConfig({
   entry: ["lib/index.ts"],
@@ -6,6 +10,16 @@ export default defineConfig({
   sourcemap: false,
   format: ["cjs", "esm"],
   dts: true,
+  bundle: true,
+  banner: {
+    js: `
+/*!
+ * grow-items v${pkg.version}
+ * Copyright ${new Date().getFullYear()}
+ * Licensed under MIT (https://github.com/StileDevs/grow-items/blob/main/LICENSE) 
+*/
+`
+  },
   outDir: "dist",
   minify: true,
   clean: true
