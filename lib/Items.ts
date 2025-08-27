d670b732025044359cd844eb35a5d6a4fc9a56f6
badewen <81739844+badewen@users.noreply.github.com>
2025-08-27 12:47:35 +0700

    Merge `flags` and `flagsCategory`

--- lib/Items.ts
+++ lib/Items.ts
@@ -61,8 +61,7 @@ export class ItemsDat {
       const item: ItemDefinition = {};
 
       item.id = this.buffer.readI32();
-      item.flags = this.buffer.readU8();
-      item.flagsCategory = this.buffer.readU8();
+      item.flags = this.buffer.readU16();
       item.type = this.buffer.readU8();
       item.materialType = this.buffer.readU8();
 
